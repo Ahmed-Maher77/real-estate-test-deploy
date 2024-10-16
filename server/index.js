abe354a9c2cb66e5a5e761bfc6d4c6fce5452164
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-10-17 01:21:27 +0300

    Added main files

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Configure CORS to allow requests from your frontend
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
+
+// Routes
+app.use('/api/user', userRoute);
+app.use('/api/residency', residencyRoute);
+
+// Start the server
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
