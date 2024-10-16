a2d9b2ad9e0de143ad9e40f9e803237c88e8f4a6
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-10-17 01:47:56 +0300

    baseUrl has been updated

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://real-estate-test-deploy.vercel.app",
     issuerBaseURL: "https://dev-u4n3gigndbkmwq7h.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
